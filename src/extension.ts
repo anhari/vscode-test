9e6a0b577dc6219540aa42f0da5d60409fbc953c
Michael Anhari <mpanhari@gmail.com>
2020-05-08 17:10:23 -0400

    Wire up commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let runAllTests = vscode.commands.registerCommand(
+    "vscode-test.runAllTests",
+    () => console.log("ran all tests")
+  );
+  let runFileTests = vscode.commands.registerCommand(
+    "vscode-test.runFileTests",
+    () => console.log("ran file tests")
+  );
+  let runLineTests = vscode.commands.registerCommand(
+    "vscode-test.runLineTests",
+    () => console.log("ran line tests")
+  );
+  let runLastTests = vscode.commands.registerCommand(
+    "vscode-test.runLastTests",
+    () => console.log("ran last tests")
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-test" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-test.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-test!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(runAllTests);
+  context.subscriptions.push(runFileTests);
+  context.subscriptions.push(runLineTests);
+  context.subscriptions.push(runLastTests);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
